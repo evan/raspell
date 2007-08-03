4896abc7679d7d5d12940d96be1a397e0f38bfb3
Evan Weaver <evan@cloudbur.st>
2007-08-03 06:33:30 +0000

    doc cleanup

--- lib/raspell_stub.rb
+++ lib/raspell_stub.rb
@@ -3,25 +3,45 @@
 raise "This file is only for documentation purposes. Require #{File.dirname(__FILE__)}/../ext/raspell} instead."
 
 # The functionality of Aspell is accessible via this class.
-class Aspell
+class ::Aspell
 
-  # Creates an Aspell instance. All parameters are optional and have default values. In most cases it is enough to create an Aspell-instance with only a dictionary (for example: <tt>Aspell.new("us")</tt>).
-  # <tt>language</tt> -  ISO639 language code plus optional ISO 3166 counry code as string (eg: "de" or "us_US").
-  # <tt>jargon</tt> - A special jargon of the selected language.
-  # <tt>size</tt> - The size of the dictionary to chose (if there are options).
-  # <tt>encoding</tt> - The encoding to use.
+  # Creates an Aspell instance. All parameters are optional and have default values. In most situations it is enough to create an Aspell-instance with only a language.
+  # * <tt>language</tt> -  ISO639 language code plus optional ISO 3166 counry code as string (eg: "de" or "us_US").
+  # * <tt>jargon</tt> - A special jargon of the selected language.
+  # * <tt>size</tt> - The size of the dictionary to chose (if there are options).
+  # * <tt>encoding</tt> - The encoding to use.
   def initialize(language, jargon, size, encoding); end
 
   # Returns a list of AspellDictInfo-objects describing each dictionary.
   def self.list_dicts; end
+  
+  # Returns a list of checker options.
+  def self.CheckerOptions; end
+  
+  # Returns a list of dictionary options.
+  def self.DictionaryOptions; end 
+  
+  # Returns a list of filter options.
+  def self.FilterOptions; end
+  
+  # Returns a list of misc options.
+  def self.MiscOptions; end 
+  
+  # Returns a list of run-together options.
+  def self.RunTogetherOptions; end 
+  
+  # Returns a list of utility options.
+  def self.UtilityOptions; end 
+  
+  
 
   # Add a word to the private dictionary.
   def add_to_personal(word); end
 
-  # Add a word to the session ignore list. The session persists for the lifetime of <tt>self</tt>
+  # Add a word to the session ignore list. The session persists for the lifetime of <tt>self</tt>.
   def add_to_session(word); end
 
-  # Check given word for correctness. Returns <tt>true</tt> or <tt>false</tt>
+  # Check given word for correctness. Returns <tt>true</tt> or <tt>false</tt>.
   def check(word); end
 
   # Check an entire file for correctness. This method requires a block, which will yield each misspelled word.
@@ -62,25 +82,21 @@ class Aspell
   # Sets the suggestion mode. Use Aspell::ULTRA, Aspell::FAST, Aspell::NORMAL, or Aspell::BADSPELLERS as the parameter.
   def suggestion_mode=(mode); end
 
-  ULTRA = Fastest mode. Look only for sound-a-likes within one edit distance. No typo analysis.
-  NORMAL = Normal speed mode. Look for sound-a-likes within two edit distances, with typo analysis. 
-  FAST = Fast mode. Look for sound-a-likes within one edit distance, with typo analysis.
-  BADSPELLERS = Slowest mode. Tailored for bad spellers.
-  CheckerOptions = A list of checker options
-  DictionaryOptions = A list of dictionary options
-  FilterOptions = A list of filter options
-  MiscOptions = A list of misc options
-  RunTogetherOptions = A list of run together options
-  UtilityOptions = A list of utility options
+  ULTRA = Fastest mode. Look only for sound-a-likes within one edit distance. No typo analysis.;
+  
+  NORMAL = Normal speed mode. Look for sound-a-likes within two edit distances, with typo analysis.;
+  
+  FAST = Fast mode. Look for sound-a-likes within one edit distance, with typo analysis.;
+  
+  BADSPELLERS = Slowest mode. Tailored for bad spellers.;
   
 end
-
 
 =begin rdoc
 Represents an Aspell dictionary. Returned by <tt>Aspell. list_dicts</tt>.     
 =end
 
-class AspellDictInfo
+class ::AspellDictInfo
 
   # Returns the code of the dictionary.
   def code; end
